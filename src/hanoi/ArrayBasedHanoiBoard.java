ae6c7ca45187280952b9cc07f877c8339452b254
Terrance J Peters <terrancejpeters@gmail.com>
2016-03-03 15:47:37 -0500

    Finished HanoiPeg, current score 78
    Nearly Complete with Hanoi Board but need to fix a few problems

--- src/hanoi/ArrayBasedHanoiBoard.java
+++ src/hanoi/ArrayBasedHanoiBoard.java
@@ -12,24 +12,90 @@ public class ArrayBasedHanoiBoard implements HanoiBoard {
 	 * Creates a {@link ArrayBasedHanoiBoard} with three empty {@link HanoiPeg}s
 	 * and {@code n} rings on peg 0.
 	 */
+	private StackBasedHanoiPeg Peg0;
+	private StackBasedHanoiPeg Peg1;
+	private StackBasedHanoiPeg Peg2;
+	private int solution;
+
 	public ArrayBasedHanoiBoard(int n) {
+		Peg0 = new StackBasedHanoiPeg();
+		for (int i = n; i > 0; i--) {
+			HanoiRing temp = new HanoiRing(i);
+			Peg0.addRing(temp);
+		}
+		Peg1 = new StackBasedHanoiPeg();
+		Peg2 = new StackBasedHanoiPeg();
+		solution = n;
 	}
 
 	@Override
 	public void doMove(HanoiMove move) throws IllegalHanoiMoveException {
 		if (!isLegalMove(move)) {
-			throw new IllegalHanoiMoveException(
-					"Could not perform illegal move.");
+			throw new IllegalHanoiMoveException("Could not perform illegal move.");
 		}
+		int from = move.getFromPeg();
+		int to = move.getToPeg();
+		StackBasedHanoiPeg TempFrom = null;
+		StackBasedHanoiPeg TempTo = null;
+		// so sorry checker
+		if (from == 0) {
+			TempFrom = Peg0;
+		} else if (from == 1) {
+			TempFrom = Peg1;
+		} else if (from == 2) {
+			TempFrom = Peg2;
+		}
+		if (to == 0) {
+			TempTo = Peg0;
+		} else if (to == 1) {
+			TempTo = Peg1;
+		} else if (to == 2) {
+			TempTo = Peg2;
+		}
+
+		TempTo.addRing(TempFrom.remove());
 	}
 
 	@Override
 	public boolean isSolved() {
-		return false;
+		if (Peg2.getSize() == solution) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	@Override
 	public boolean isLegalMove(HanoiMove move) {
-		return false;
+		int from = move.getFromPeg();
+		int to = move.getToPeg();
+		StackBasedHanoiPeg TempFrom = null;
+		StackBasedHanoiPeg TempTo = null;
+		if (from == 0) {
+			TempFrom = Peg0;
+		} else if (from == 1) {
+			TempFrom = Peg1;
+		} else if (from == 2) {
+			TempFrom = Peg2;
+		} else {
+			return false;
+		}
+		if (to == 0) {
+			TempTo = Peg0;
+		} else if (to == 1) {
+			TempTo = Peg1;
+		} else if (to == 2) {
+			TempTo = Peg2;
+		} else {
+			return false;
+		}
+		if (TempFrom.getSize() == 0) {
+			return false;
+		}
+		if (TempTo.getTopRing().getSize() < TempFrom.getTopRing().getSize()) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
--- src/hanoi/StackBasedHanoiPeg.java
+++ src/hanoi/StackBasedHanoiPeg.java
@@ -1,5 +1,7 @@
 package hanoi;
 
+import structures.LinkedStack;
+
 /**
  * A {@link StackBasedHanoiPeg} is an implementation of {@link HanoiPeg}.
  * 
@@ -7,28 +9,50 @@ package hanoi;
  */
 public class StackBasedHanoiPeg implements HanoiPeg {
 
+	private LinkedStack<HanoiRing> Peg;
 	/**
 	 * Creates a new {@link StackBasedHanoiPeg} that has no rings.
 	 */
 	public StackBasedHanoiPeg() {
+		Peg = new LinkedStack<HanoiRing>();
 	}
 
 	@Override
 	public void addRing(HanoiRing ring) throws IllegalHanoiMoveException {
+		if (Peg.getSize() == 0){
+			Peg.push(ring);
+		}
+		else if (Peg.peek().getSize() > ring.getSize()){
+			Peg.push(ring);
+		}
+		else{
+			throw new IllegalHanoiMoveException("Bad Move! Ring to add is greater than top ring...");
+		}
 	}
 
 	@Override
 	public HanoiRing remove() throws IllegalHanoiMoveException {
-		return null;
+		if (Peg.getSize() == 0){
+			throw new IllegalHanoiMoveException("Can't get Top Ring, Peg is Empty!");
+		}
+		return Peg.pop();
 	}
 
 	@Override
 	public HanoiRing getTopRing() throws IllegalHanoiMoveException {
-		return null;
+		if (Peg.getSize() == 0)
+			throw new IllegalHanoiMoveException("Can't get Top Ring, Peg is Empty!");
+		return Peg.peek();
 	}
 
 	@Override
 	public boolean hasRings() {
-		return false;
+		if (Peg.getSize() > 0)
+			return true;
+		else
+			return false;
+	}
+	public int getSize(){
+		return Peg.getSize();
 	}
 }
