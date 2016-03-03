92b6a3273c5b5623c153003b7347a939e189a4e7
Terrance J Peters <terrancejpeters@gmail.com>
2016-03-03 10:32:33 -0500

    Completed List Implentation
    Figured Out problem with get(index) method and append(elem) method,
    pretty much makes Implement complete. Also added Iterator

--- src/structures/ListImplementation.java
+++ src/structures/ListImplementation.java
@@ -9,6 +9,14 @@ public class ListImplementation<T> implements ListInterface<T> {
 	private Node<T> first = null;
 	private Node<T> last = null;
 
+	public ListImplementation(){
+	}
+	public ListImplementation(T data){
+		first = new Node<T>(data, null);
+	}
+	public ListImplementation(Node<T> head){
+		first = head;
+	}
 	@Override
 	public int size() {
 		// TODO Auto-generated method stub
@@ -27,26 +35,32 @@ public class ListImplementation<T> implements ListInterface<T> {
 
 	@Override
 	public T get(int n) throws NoSuchElementException {
-		Node<T> temp = first;
-		if (n > size) {
+		Iterator<T> it = new listIterator<T>(first);
+		if (n > size-1 || n < 0) {
 			throw new NoSuchElementException("Greater than size");
 		}
+		T temp = it.next();
 		int i = 0;
-		while (i < n) {
-			temp = temp.getNext();
+		while (i < n && it.hasNext()) {
+			temp = it.next();
 			i++;
 		}
-		return temp.getData();
+		return temp;
 	}
 
 	@Override
 	public ListInterface<T> append(T elem) {
 		// TODO Auto-generated method stub
+		ListImplementation<T> rval = this;
 		size++;
+		if (elem == null){
+			throw new NullPointerException();
+		}
 		if (first == null) {
-			first = new Node<T>(elem, last);
+			first = new Node<T>(elem, null);
 		} else if (last == null) {
 			last = new Node<T>(elem, null);
+			first.setNext(last);
 		} else {
 			Node<T> temp = new Node<T>(elem, null);
 			last.setNext(temp);
@@ -58,7 +72,7 @@ public class ListImplementation<T> implements ListInterface<T> {
 	@Override
 	public Iterator<T> iterator() {
 		// TODO Auto-generated method stub
-			return new Iterator<T>(first);
+			return new listIterator<T>(first);
 			//Make a new list iterator
 	}
 
